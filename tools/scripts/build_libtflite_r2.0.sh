13ff9aba1705dd61482723a16e0403421e89c2d8
terryky <terryky1220@gmail.com>
2019-11-30 14:03:24 +0900

    added build script for TensorFlow Lite 2.0

---
+++ tools/scripts/build_libtflite_r2.0.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -e
+#set -x
+
+export TENSORFLOW_VER=r2.0
+export TENSORFLOW_DIR=`pwd`/tensorflow_${TENSORFLOW_VER}
+
+git clone https://github.com/tensorflow/tensorflow.git ${TENSORFLOW_DIR}
+
+cd ${TENSORFLOW_DIR}
+git checkout ${TENSORFLOW_VER}
+
+echo "----------------------------------------------------"
+echo " (configure) press ENTER-KEY several times.         "
+echo "----------------------------------------------------"
+./configure
+
+# clean up bazel cache, just in case.
+bazel clean
+
+# download all the build dependencies.
+./tensorflow/lite/tools/make/download_dependencies.sh 2>&1 | tee -a log_download_dependencies.txt
+
+
+# apply a patch regarding "undefined reference to `rdft2d'"
+#
+#  ./tensorflow/lite/tools/make/Makefile
+#    $(wildcard tensorflow/lite/tools/make/downloads/fft2d/fftsg.c) \
+#  + $(wildcard tensorflow/lite/tools/make/downloads/fft2d/fftsg2d.c) \
+#    $(wildcard tensorflow/lite/tools/make/downloads/flatbuffers/src/util.cpp)
+sed -e '/fftsg.c/a tensorflow\/lite\/tools\/make\/downloads\/fft2d\/fftsg2d.c \\' -i ./tensorflow/lite/tools/make/Makefile
+
+# build TensorFlow Lite library (libtensorflow-lite.a)
+make -j 4  -f ./tensorflow/lite/tools/make/Makefile BUILD_WITH_NNAPI=false 2>&1 | tee -a log_build_libtflite.txt
+
+# build GPU Delegate library (libtensorflowlite_gpu_gl.so)
+bazel build -s -c opt --copt="-DMESA_EGL_NO_X11_HEADERS" tensorflow/lite/delegates/gpu:libtensorflowlite_gpu_gl.so 2>&1 | tee -a log_build_delegate.txt
+
+
+
+echo "----------------------------------------------------"
+echo " build success."
+echo "----------------------------------------------------"
+
+cd ${TENSORFLOW_DIR}
+ls -l tensorflow/lite/tools/make/gen/linux_x86_64/lib/
+ls -l bazel-bin/tensorflow/lite/delegates/gpu/
+
+
+
