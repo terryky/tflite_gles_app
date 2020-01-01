c867652ae5406a25d87715fe7fd2173b5eae725e
terryky <terryky1220@gmail.com>
2020-01-01 23:17:20 +0900

    added build script

---
+++ tools/scripts/tf2.1/clone_and_download_dependencies_tflite_r2.1.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+#set -x
+
+export TENSORFLOW_VER=r2.1
+export TENSORFLOW_DIR=`pwd`/tensorflow_${TENSORFLOW_VER}
+
+git clone https://github.com/tensorflow/tensorflow.git ${TENSORFLOW_DIR}
+
+cd ${TENSORFLOW_DIR}
+git checkout ${TENSORFLOW_VER}
+
+
+# apply patch for GPU Delegate
+export SCRIPT_DIR=`dirname $0`
+PATCH_FILE=${SCRIPT_DIR}/tensorflow_tf21_tflite_download_dependencies.diff
+patch -p1 < ${PATCH_FILE}
+
+# download all the build dependencies.
+./tensorflow/lite/tools/make/download_dependencies.sh
+
