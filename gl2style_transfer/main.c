4330d75524854e49062a6b27bf9c691c237a0747
terryky <terryky1220@gmail.com>
2020-01-12 20:54:39 +0900

    added gl2style_transer

---
+++ gl2style_transfer/main.c
@@ -0,0 +1,340 @@
+/* ------------------------------------------------ *
+ * The MIT License (MIT)
+ * Copyright (c) 2019 terryky1220@gmail.com
+ * ------------------------------------------------ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <float.h>
+#include <GLES2/gl2.h>
+#include "util_egl.h"
+#include "util_debugstr.h"
+#include "util_pmeter.h"
+#include "util_texture.h"
+#include "util_render2d.h"
+#include "tflite_style_transfer.h"
+#include "camera_capture.h"
+
+#define UNUSED(x) (void)(x)
+
+#if defined (USE_INPUT_CAMERA_CAPTURE)
+static void
+update_capture_texture (int texid)
+{
+    int   cap_w, cap_h;
+    void *cap_buf;
+
+    get_capture_dimension (&cap_w, &cap_h);
+    get_capture_buffer (&cap_buf);
+
+    if (cap_buf)
+    {
+        glBindTexture (GL_TEXTURE_2D, texid);
+        glTexSubImage2D (GL_TEXTURE_2D, 0, 0, 0, cap_w, cap_h, GL_RGBA, GL_UNSIGNED_BYTE, cap_buf);
+    }
+}
+#endif
+
+/* resize image to DNN network input size and convert to fp32. */
+void
+feed_style_transfer_image(int is_predict, int texid, int win_w, int win_h)
+{
+#if defined (USE_INPUT_CAMERA_CAPTURE)
+    update_capture_texture (texid);
+#endif
+
+    int x, y, w, h;
+    float *buf_fp32;
+    unsigned char *buf_ui8, *pui8;
+
+    if (is_predict)
+        buf_fp32 = (float *)get_style_predict_input_buf (&w, &h);
+    else
+        buf_fp32 = (float *)get_style_transfer_content_input_buf (&w, &h);
+
+    pui8 = buf_ui8 = (unsigned char *)malloc(w * h * 4);
+
+    draw_2d_texture (texid, 0, win_h - h, w, h, 1);
+
+    glPixelStorei (GL_PACK_ALIGNMENT, 1);
+    glReadPixels (0, 0, w, h, GL_RGBA, GL_UNSIGNED_BYTE, buf_ui8);
+
+    /* convert UI8 [0, 255] ==> FP32 [-1, 1] */
+    float mean = 128.0f;
+    float std  = 128.0f;
+    for (y = 0; y < h; y ++)
+    {
+        for (x = 0; x < w; x ++)
+        {
+            int r = *buf_ui8 ++;
+            int g = *buf_ui8 ++;
+            int b = *buf_ui8 ++;
+            buf_ui8 ++;          /* skip alpha */
+            *buf_fp32 ++ = (float)(r - mean) / std;
+            *buf_fp32 ++ = (float)(g - mean) / std;
+            *buf_fp32 ++ = (float)(b - mean) / std;
+        }
+    }
+
+    free (pui8);
+    return;
+}
+
+void
+store_style_predict (style_predict_t *style)
+{
+    int size = style->size;
+    float *param = style->param;
+    float *store_param = (float *)calloc (1, size * sizeof(float));
+
+    style->param = store_param;
+
+    while (size --> 0)
+    {
+        *store_param ++ = *param ++;
+    }
+}
+
+void
+feed_blend_style (style_predict_t *style0, style_predict_t *style1, float ratio)
+{
+    int size;
+    float *s0 = style0->param;
+    float *s1 = style1->param;
+    float *d = get_style_transfer_style_input_buf (&size);
+
+    if (style0->size != size || style1->size != size)
+    {
+        fprintf (stderr, "ERR: %s(%d)\n", __FILE__, __LINE__);
+        return;
+    }
+    
+    for (int i = 0; i < size; i ++)
+    {
+        float src0 = *s0;
+        float src1 = *s1;
+        float src  = (ratio * src1) + ((1.0f - ratio) * src0);
+
+        *d ++ = src;
+        s0 ++;
+        s1 ++;
+    }
+}
+
+
+/* upload style transfered image to OpenGLES texture */
+static int
+update_style_transfered_texture (style_transfer_t *transfer)
+{
+    static int s_texid = 0;
+    static uint8_t *s_texbuf = NULL;
+    int img_w = transfer->w;
+    int img_h = transfer->h;
+
+    if (s_texid == 0)
+    {
+        s_texbuf = (uint8_t *)calloc (1, img_w * img_h * 4);
+        s_texid  = create_2d_texture (s_texbuf, img_w, img_h);
+    }
+
+    uint8_t *d = s_texbuf;
+    float   *s = transfer->img;
+
+    for (int y = 0; y < img_h; y ++)
+    {
+        for (int x = 0; x < img_w; x ++)
+        {
+            float r = *s ++;
+            float g = *s ++;
+            float b = *s ++;
+            *d ++ = (uint8_t)(r * 255);
+            *d ++ = (uint8_t)(g * 255);
+            *d ++ = (uint8_t)(b * 255);
+            *d ++ = 0xFF;
+        }
+    }
+
+    glBindTexture (GL_TEXTURE_2D, s_texid);
+    glTexSubImage2D (GL_TEXTURE_2D, 0, 0, 0, img_w, img_h, GL_RGBA, GL_UNSIGNED_BYTE, s_texbuf);
+
+    return s_texid;
+}
+
+
+/* Adjust the texture size to fit the window size
+ *
+ *                      Portrait
+ *     Landscape        +------+
+ *     +-+------+-+     +------+
+ *     | |      | |     |      |
+ *     | |      | |     |      |
+ *     +-+------+-+     +------+
+ *                      +------+
+ */
+static void
+adjust_texture (int win_w, int win_h, int texw, int texh, 
+                int *dx, int *dy, int *dw, int *dh)
+{
+    float win_aspect = (float)win_w / (float)win_h;
+    float tex_aspect = (float)texw  / (float)texh;
+    float scale;
+    float scaled_w, scaled_h;
+    float offset_x, offset_y;
+
+    if (win_aspect > tex_aspect)
+    {
+        scale = (float)win_h / (float)texh;
+        scaled_w = scale * texw;
+        scaled_h = scale * texh;
+        offset_x = (win_w - scaled_w) * 0.5f;
+        offset_y = 0;
+    }
+    else
+    {
+        scale = (float)win_w / (float)texw;
+        scaled_w = scale * texw;
+        scaled_h = scale * texh;
+        offset_x = 0;
+        offset_y = (win_h - scaled_h) * 0.5f;
+    }
+
+    *dx = (int)offset_x;
+    *dy = (int)offset_y;
+    *dw = (int)scaled_w;
+    *dh = (int)scaled_h;
+}
+
+
+/*--------------------------------------------------------------------------- *
+ *      M A I N    F U N C T I O N
+ *--------------------------------------------------------------------------- */
+int
+main(int argc, char *argv[])
+{
+    char input_name_default[] = "pakutaso_sotsugyou.jpg";
+    char *input_name = input_name_default;
+    char input_style_name_default[] = "style_newspaper.jpg";
+    char *input_style_name = input_style_name_default;
+    int count;
+    int win_w = 960;
+    int win_h = 540;
+    int texid;
+    int texw, texh, draw_x, draw_y, draw_w, draw_h;
+    float style_ratio = 0.0f;
+    double ttime[10] = {0}, interval, invoke_ms;
+    UNUSED (argc);
+    UNUSED (*argv);
+
+    /* gl2style_transfer [content_file_name] [style_file_name] */
+    if (argc > 1)
+        input_name = argv[1];
+
+    if (argc > 2)
+        input_style_name = argv[2];
+
+    egl_init_with_platform_window_surface (2, 0, 0, 0, win_w, win_h);
+
+    init_2d_renderer (win_w, win_h);
+    init_pmeter (win_w, win_h, 500);
+    init_dbgstr (win_w, win_h);
+
+    init_tflite_style_transfer ();
+
+#if defined (USE_GL_DELEGATE) || defined (USE_GPU_DELEGATEV2)
+    /* we need to recover framebuffer because GPU Delegate changes the context */
+    glBindFramebuffer (GL_FRAMEBUFFER, 0);
+    glViewport (0, 0, win_w, win_h);
+#endif
+
+#if defined (USE_INPUT_CAMERA_CAPTURE)
+    /* initialize V4L2 capture function */
+    if (init_capture () == 0)
+    {
+        /* allocate texture buffer for captured image */
+        get_capture_dimension (&texw, &texh);
+        texid = create_2d_texture (NULL, texw, texh);
+        start_capture ();
+    }
+    else
+#endif
+    load_jpg_texture (input_name, &texid, &texw, &texh);
+    adjust_texture (win_w, win_h, texw, texh, &draw_x, &draw_y, &draw_w, &draw_h);
+
+    glClearColor (0.f, 0.f, 0.f, 1.0f);
+
+    /* --------------------------------------- *
+     *  Style prediction
+     * --------------------------------------- */
+    style_predict_t style_predict[2] = {0};
+    {
+        int w, h;
+        int style_texid;
+        load_jpg_texture (input_style_name, &style_texid, &w, &h);
+
+        /* predict style of original image */
+        glClear (GL_COLOR_BUFFER_BIT);
+        feed_style_transfer_image (1, texid, win_w, win_h);
+        invoke_style_predict (&style_predict[0]);
+        store_style_predict (&style_predict[0]);
+
+        /* predict style of target image */
+        glClear (GL_COLOR_BUFFER_BIT);
+        feed_style_transfer_image (1, style_texid, win_w, win_h);
+        invoke_style_predict (&style_predict[1]);
+        store_style_predict (&style_predict[1]);
+    }
+
+    /* --------------------------------------- *
+     *  Style transfer
+     * --------------------------------------- */
+    for (count = 0; ; count ++)
+    {
+        style_transfer_t style_transfered = {0};
+        char strbuf[512];
+
+        PMETER_RESET_LAP ();
+        PMETER_SET_LAP ();
+
+        ttime[1] = pmeter_get_time_ms ();
+        interval = (count > 0) ? ttime[1] - ttime[0] : 0;
+        ttime[0] = ttime[1];
+
+        glClear (GL_COLOR_BUFFER_BIT);
+
+        /* 
+         *  update style parameter blend ratio.
+         *      0.0: apply 100[%] style of original image.
+         *      1.0: apply 100[%] style of target image.
+         */
+        style_ratio += 0.1f;
+        if (style_ratio > 1.0f)
+            style_ratio = 0.0f;
+
+        /* feed style parameter and original image */
+        feed_blend_style (&style_predict[0], &style_predict[1], style_ratio);
+        feed_style_transfer_image (0, texid, win_w, win_h);
+
+        /* invoke pose estimation using TensorflowLite */
+        ttime[2] = pmeter_get_time_ms ();
+        invoke_style_transfer (&style_transfered);
+        ttime[3] = pmeter_get_time_ms ();
+        invoke_ms = ttime[3] - ttime[2];
+
+        /* visualize the style transform results. */
+        glClear (GL_COLOR_BUFFER_BIT);
+        int transfered_texid = update_style_transfered_texture (&style_transfered);
+        draw_2d_texture (transfered_texid,  draw_x, draw_y, draw_w, draw_h, 0);
+
+        draw_pmeter (0, 40);
+
+        sprintf (strbuf, "Interval:%5.1f [ms]\nTFLite  :%5.1f [ms]\nstyle_ratio=%.1f", 
+                                interval, invoke_ms, style_ratio);
+        draw_dbgstr (strbuf, 10, 10);
+
+        egl_swap();
+    }
+
+    return 0;
+}
+
---
+++ gl2style_transfer/tflite_style_transfer.cpp
@@ -0,0 +1,291 @@
+/* ------------------------------------------------ *
+ * The MIT License (MIT)
+ * Copyright (c) 2019 terryky1220@gmail.com
+ * ------------------------------------------------ */
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/model.h"
+#include "tensorflow/lite/optional_debug_tools.h"
+#if defined (USE_GL_DELEGATE)
+#include "tensorflow/lite/delegates/gpu/gl_delegate.h"
+#endif
+#if defined (USE_GPU_DELEGATEV2)
+#include "tensorflow/lite/delegates/gpu/delegate.h"
+#endif
+#include "tflite_style_transfer.h"
+#include <float.h>
+
+#define STYLE_PREDICT_MODEL_PATH  "./style_transfer_model/style_predict_quantized_256.tflite"
+#define STYLE_TRANSFER_MODEL_PATH "./style_transfer_model/style_transfer_quantized_dynamic.tflite"
+
+using namespace std;
+using namespace tflite;
+
+typedef struct tflite_interpreter_t
+{
+    unique_ptr<FlatBufferModel>     model;
+    unique_ptr<Interpreter>         interpreter;
+    ops::builtin::BuiltinOpResolver resolver;
+} tflite_interpreter_t;
+
+
+static tflite_interpreter_t s_interpreter_style_predict;
+static float    *s_style_predict_in_ptr;
+static int      s_style_predict_img_w = 0;
+static int      s_style_predict_img_h = 0;
+
+
+static tflite_interpreter_t s_interpreter_style_transfer;
+static float    *s_style_transfer_style_in_ptr;
+static float    *s_style_transfer_content_in_ptr;
+static int      s_style_transfer_style_dim;
+static int      s_style_transfer_img_w = 0;
+static int      s_style_transfer_img_h = 0;
+
+
+
+static void
+print_tensor_dim (unique_ptr<Interpreter> &interpreter, int tensor_id)
+{
+    TfLiteIntArray *dim = interpreter->tensor(tensor_id)->dims;
+
+    for (int i = 0; i < dim->size; i ++)
+    {
+        if (i > 0)
+            fprintf (stderr, "x");
+        fprintf (stderr, "%d", dim->data[i]);
+    }
+    fprintf (stderr, "\n");
+}
+
+static void
+print_tensor_info (unique_ptr<Interpreter> &interpreter)
+{
+    int i, idx;
+    int in_size  = interpreter->inputs().size();
+    int out_size = interpreter->outputs().size();
+
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    fprintf (stderr, "tensors size     : %zu\n", interpreter->tensors_size());
+    fprintf (stderr, "nodes   size     : %zu\n", interpreter->nodes_size());
+    fprintf (stderr, "number of inputs : %d\n", in_size);
+    fprintf (stderr, "number of outputs: %d\n", out_size);
+    fprintf (stderr, "input(0) name    : %s\n", interpreter->GetInputName(0));
+
+    fprintf (stderr, "\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    fprintf (stderr, "                     name                     bytes  type  scale   zero_point\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    int t_size = interpreter->tensors_size();
+    for (i = 0; i < t_size; i++) 
+    {
+        fprintf (stderr, "Tensor[%2d] %-32s %8zu, %2d, %f, %3d\n", i,
+            interpreter->tensor(i)->name, 
+            interpreter->tensor(i)->bytes,
+            interpreter->tensor(i)->type,
+            interpreter->tensor(i)->params.scale,
+            interpreter->tensor(i)->params.zero_point);
+    }
+
+    fprintf (stderr, "\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    fprintf (stderr, " Input Tensor Dimension\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    for (i = 0; i < in_size; i ++)
+    {
+        idx = interpreter->inputs()[i];
+        fprintf (stderr, "Tensor[%2d]: ", idx);
+        print_tensor_dim (interpreter, idx);
+    }
+
+    fprintf (stderr, "\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    fprintf (stderr, " Output Tensor Dimension\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    for (i = 0; i < out_size; i ++)
+    {
+        idx = interpreter->outputs()[i];
+        fprintf (stderr, "Tensor[%2d]: ", idx);
+        print_tensor_dim (interpreter, idx);
+    }
+
+    fprintf (stderr, "\n");
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+    PrintInterpreterState(interpreter.get());
+    fprintf (stderr, "-----------------------------------------------------------------------------\n");
+}
+
+
+static int
+create_interpreter(tflite_interpreter_t *p, const char *model_path)
+{
+    p->model = FlatBufferModel::BuildFromFile(model_path);
+    if (!p->model)
+    {
+        fprintf (stderr, "ERR: %s(%d)\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    InterpreterBuilder(*(p->model), p->resolver)(&(p->interpreter));
+    if (!p->interpreter)
+    {
+        fprintf (stderr, "ERR: %s(%d)\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    p->interpreter->SetNumThreads(4);
+    if (p->interpreter->AllocateTensors() != kTfLiteOk)
+    {
+        fprintf (stderr, "ERR: %s(%d)\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+#if 1 /* for debug */
+    print_tensor_info (p->interpreter);
+#endif
+
+    return 0;
+}
+
+static int
+init_tflite_style_predict(tflite_interpreter_t *p, const char *model_path)
+{
+    create_interpreter (p, model_path);
+
+    s_style_predict_in_ptr  = p->interpreter->typed_input_tensor<float>(0);
+
+    /* input image dimention */
+    int input_idx = p->interpreter->inputs()[0];
+    TfLiteIntArray *in_dim = p->interpreter->tensor(input_idx)->dims;
+    s_style_predict_img_w = in_dim->data[2];
+    s_style_predict_img_h = in_dim->data[1];
+    fprintf (stderr, "input image size: (%d, %d)\n", s_style_predict_img_w, s_style_predict_img_h);
+
+    /* style output  dimention */
+    int output_idx = p->interpreter->outputs()[0];
+    TfLiteIntArray *out_dim = p->interpreter->tensor(output_idx)->dims;
+    int s0 = out_dim->data[0];
+    int s1 = out_dim->data[1];
+    int s2 = out_dim->data[2];
+    int s3 = out_dim->data[3];
+    fprintf (stderr, "style output dim: (%dx%dx%dx%d)\n", s0, s1, s2, s3);
+
+    return 0;
+}
+
+static int
+init_tflite_style_trans(tflite_interpreter_t *p, const char *model_path)
+{
+    create_interpreter (p, model_path);
+
+    s_style_transfer_content_in_ptr = p->interpreter->typed_input_tensor<float>(0);
+    s_style_transfer_style_in_ptr   = p->interpreter->typed_input_tensor<float>(1);
+
+    /* input image dimention */
+    int input_content_idx = p->interpreter->inputs()[0];
+    TfLiteIntArray *in_content_dim = p->interpreter->tensor(input_content_idx)->dims;
+    s_style_transfer_img_w = in_content_dim->data[2];
+    s_style_transfer_img_h = in_content_dim->data[1];
+    fprintf (stderr, "input image size: [%d](%d, %d)\n", input_content_idx, s_style_transfer_img_w, s_style_transfer_img_h);
+
+    int input_style_idx = p->interpreter->inputs()[1];
+    TfLiteIntArray *in_style_dim = p->interpreter->tensor(input_style_idx)->dims;
+    s_style_transfer_style_dim = in_style_dim->data[3];
+    fprintf (stderr, "input image size: [%d](%d)\n", input_style_idx, s_style_transfer_style_dim);
+
+    int output_style_idx = p->interpreter->outputs()[0];
+    TfLiteIntArray *out_dim = p->interpreter->tensor(output_style_idx)->dims;
+    int s0 = out_dim->data[0];
+    int s1 = out_dim->data[1];
+    int s2 = out_dim->data[2];
+    int s3 = out_dim->data[3];
+    fprintf (stderr, "style output dim: (%dx%dx%dx%d)\n", s0, s1, s2, s3);
+
+    return 0;
+}
+
+
+int
+init_tflite_style_transfer ()
+{
+    init_tflite_style_predict (&s_interpreter_style_predict,  STYLE_PREDICT_MODEL_PATH);
+    init_tflite_style_trans   (&s_interpreter_style_transfer, STYLE_TRANSFER_MODEL_PATH);
+
+    return 0;
+}
+
+void *
+get_style_predict_input_buf (int *w, int *h)
+{
+    *w = s_style_predict_img_w;
+    *h = s_style_predict_img_h;
+    return s_style_predict_in_ptr;
+}
+
+void *
+get_style_transfer_style_input_buf (int *size)
+{
+    s_style_transfer_style_in_ptr = s_interpreter_style_transfer.interpreter->typed_input_tensor<float>(1);
+
+    *size = s_style_transfer_style_dim;
+    return s_style_transfer_style_in_ptr;
+}
+
+void *
+get_style_transfer_content_input_buf (int *w, int *h)
+{
+    s_style_transfer_content_in_ptr = s_interpreter_style_transfer.interpreter->typed_input_tensor<float>(0);
+
+    *w = s_style_transfer_img_w;
+    *h = s_style_transfer_img_h;
+    return s_style_transfer_content_in_ptr;
+}
+
+
+/* -------------------------------------------------- *
+ * Invoke TensorFlow Lite
+ * -------------------------------------------------- */
+int
+invoke_style_predict (style_predict_t *predict_result)
+{
+    if (s_interpreter_style_predict.interpreter->Invoke() != kTfLiteOk)
+    {
+        fprintf (stderr, "ERR: %s(%d)\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    int output_idx = s_interpreter_style_predict.interpreter->outputs()[0];
+    TfLiteIntArray *out_dim = s_interpreter_style_predict.interpreter->tensor(output_idx)->dims;
+    //int s0 = out_dim->data[0];
+    //int s1 = out_dim->data[1];
+    //int s2 = out_dim->data[2];
+    int s3 = out_dim->data[3];
+
+    predict_result->size  = s3;
+    predict_result->param = s_interpreter_style_predict.interpreter->typed_output_tensor<float>(0); 
+    
+    return 0;
+}
+
+
+int
+invoke_style_transfer (style_transfer_t *transfered_result)
+{
+    if (s_interpreter_style_transfer.interpreter->Invoke() != kTfLiteOk)
+    {
+        fprintf (stderr, "ERR: %s(%d)\n", __FILE__, __LINE__);
+        return -1;
+    }
+
+    int output_idx = s_interpreter_style_transfer.interpreter->outputs()[0];
+    TfLiteIntArray *out_dim = s_interpreter_style_transfer.interpreter->tensor(output_idx)->dims;
+    //int s0 = out_dim->data[0];
+    int s1 = out_dim->data[1];
+    int s2 = out_dim->data[2];
+    //int s3 = out_dim->data[3];
+    
+    transfered_result->h = s1;
+    transfered_result->w = s2;
+    transfered_result->img = s_interpreter_style_transfer.interpreter->typed_output_tensor<float>(0); 
+
+    return 0;
+}
