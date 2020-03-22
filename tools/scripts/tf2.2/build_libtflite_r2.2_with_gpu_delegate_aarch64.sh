2b418f028c2a979b74e1e2657ed523cba362f24a
terryky <terryky1220@gmail.com>
2020-03-22 20:02:20 +0900

    added build script for tensorflow 2.2

---
+++ tools/scripts/tf2.2/build_libtflite_r2.2_with_gpu_delegate_aarch64.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+set -e
+#set -x
+
+export TENSORFLOW_VER=r2.2
+export TENSORFLOW_DIR=`pwd`/tensorflow_${TENSORFLOW_VER}
+export SCRIPT_DIR=`dirname $(realpath $0)`
+
+git clone https://github.com/tensorflow/tensorflow.git ${TENSORFLOW_DIR}
+
+cd ${TENSORFLOW_DIR}
+git checkout ${TENSORFLOW_VER}
+
+
+# apply patch for GPU Delegate
+PATCH_FILE=${SCRIPT_DIR}/tensorflow_tf22_enable_gpu_delegate.diff
+patch -p1 < ${PATCH_FILE}
+
+
+# clean up bazel cache, just in case.
+bazel clean
+
+echo "----------------------------------------------------"
+echo " (configure) press ENTER-KEY several times.         "
+echo "----------------------------------------------------"
+./configure
+
+
+# download all the build dependencies.
+./tensorflow/lite/tools/make/download_dependencies.sh 2>&1 | tee -a log_download_dependencies.txt
+
+
+# build GPU Delegate library (libdelegate.a)
+bazel build -s -c opt --copt="-DMESA_EGL_NO_X11_HEADERS" tensorflow/lite/delegates/gpu:libtensorflowlite_gpu_delegate.so 2>&1 | tee -a log_build_delegate.txt
+
+
+# reuse bazel products for make.
+cd ${TENSORFLOW_DIR}
+ln -s ./bazel-bin/../../../external .
+cp bazel-out/k8-opt/bin/tensorflow/lite/delegates/gpu/cl/compiled_program_cache_generated.h tensorflow/lite/delegates/gpu/cl/
+
+cp -rf ./external/flatbuffers/* tensorflow/lite/tools/make/downloads/flatbuffers/
+cp -rf ./external/com_google_absl/* tensorflow/lite/tools/make/downloads/absl/
+
+# build TensorFlow Lite library (libtensorflow-lite.a)
+make -j 4  -f ./tensorflow/lite/tools/make/Makefile BUILD_WITH_NNAPI=false TARGET=aarch64 2>&1 | tee -a log_build_libtflite_gpu_delegate.txt
+
+
+echo "----------------------------------------------------"
+echo " build success."
+echo "----------------------------------------------------"
+
+cd ${TENSORFLOW_DIR}
+ls -l tensorflow/lite/tools/make/gen/linux_aarch64/lib/
+ls -l bazel-bin/tensorflow/lite/delegates/gpu/
+
+
+
