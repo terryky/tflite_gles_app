e165afffd323483a5824ed7a937ee29cd9d17b48
terryky <terryky1220@gmail.com>
2020-06-08 22:17:20 +0900

    support decode video stream

--- gl2style_transfer/camera_capture.h
+++ gl2style_transfer/camera_capture.h
@@ -5,10 +5,13 @@
 #ifndef CAMERA_CAPTURE_H_
 #define CAMERA_CAPTURE_H_
 
+#include <stdint.h>
 
 int init_capture ();
 int get_capture_dimension (int *width, int *height);
+int get_capture_pixformat (uint32_t *pixformat);
 int get_capture_buffer (void ** buf);
+
 int start_capture ();
 
 
--- gl2style_transfer/main.c
+++ gl2style_transfer/main.c
@@ -15,56 +15,142 @@
 #include "util_render2d.h"
 #include "tflite_style_transfer.h"
 #include "camera_capture.h"
+#include "video_decode.h"
 
 #define UNUSED(x) (void)(x)
 
+
 #if defined (USE_INPUT_CAMERA_CAPTURE)
 static void
-update_capture_texture (int texid)
+update_capture_texture (texture_2d_t *captex)
 {
-    int   cap_w, cap_h;
-    void *cap_buf;
+    int      cap_w, cap_h;
+    uint32_t cap_fmt;
+    void     *cap_buf;
 
     get_capture_dimension (&cap_w, &cap_h);
+    get_capture_pixformat (&cap_fmt);
     get_capture_buffer (&cap_buf);
-
     if (cap_buf)
     {
-        glBindTexture (GL_TEXTURE_2D, texid);
-        glTexSubImage2D (GL_TEXTURE_2D, 0, 0, 0, cap_w, cap_h, GL_RGBA, GL_UNSIGNED_BYTE, cap_buf);
+        int texw = cap_w;
+        int texh = cap_h;
+        int texfmt = GL_RGBA;
+        switch (cap_fmt)
+        {
+        case pixfmt_fourcc('Y', 'U', 'Y', 'V'):
+            texw = cap_w / 2;
+            break;
+        default:
+            break;
+        }
+
+        glBindTexture (GL_TEXTURE_2D, captex->texid);
+        glTexSubImage2D (GL_TEXTURE_2D, 0, 0, 0, texw, texh, texfmt, GL_UNSIGNED_BYTE, cap_buf);
     }
 }
+
+static int
+init_capture_texture (texture_2d_t *captex)
+{
+    int      cap_w, cap_h;
+    uint32_t cap_fmt;
+
+    get_capture_dimension (&cap_w, &cap_h);
+    get_capture_pixformat (&cap_fmt);
+
+    create_2d_texture_ex (captex, NULL, cap_w, cap_h, cap_fmt);
+    start_capture ();
+
+    return 0;
+}
+
 #endif
+
+#if defined (USE_INPUT_VIDEO_DECODE)
+static void
+update_video_texture (texture_2d_t *captex)
+{
+    int   video_w, video_h;
+    uint32_t video_fmt;
+    void *video_buf;
+
+    get_video_dimension (&video_w, &video_h);
+    get_video_pixformat (&video_fmt);
+    get_video_buffer (&video_buf);
+
+    if (video_buf)
+    {
+        int texw = video_w;
+        int texh = video_h;
+        int texfmt = GL_RGBA;
+        switch (video_fmt)
+        {
+        case pixfmt_fourcc('Y', 'U', 'Y', 'V'):
+            texw = video_w / 2;
+            break;
+        default:
+            break;
+        }
+
+        glBindTexture (GL_TEXTURE_2D, captex->texid);
+        glTexSubImage2D (GL_TEXTURE_2D, 0, 0, 0, texw, texh, texfmt, GL_UNSIGNED_BYTE, video_buf);
+    }
+}
+
+static int
+init_video_texture (texture_2d_t *captex, const char *fname)
+{
+    int      vid_w, vid_h;
+    uint32_t vid_fmt;
+
+    open_video_file (fname);
+
+    get_video_dimension (&vid_w, &vid_h);
+    get_video_pixformat (&vid_fmt);
+
+    create_2d_texture_ex (captex, NULL, vid_w, vid_h, vid_fmt);
+    start_video_decode ();
+
+    return 0;
+}
+#endif /* USE_INPUT_VIDEO_DECODE */
+
 
 /* resize image to DNN network input size and convert to fp32. */
 void
-feed_style_transfer_image(int is_predict, int texid, int win_w, int win_h)
+feed_style_transfer_image(int is_predict, texture_2d_t *srctex, int win_w, int win_h)
 {
     int x, y, w, h;
     float *buf_fp32;
-    unsigned char *buf_ui8, *pui8;
+    unsigned char *buf_ui8 = NULL;
+    static int buf_w = 0, buf_h = 0;
+    static unsigned char *pui8 = NULL;
 
     if (is_predict)
         buf_fp32 = (float *)get_style_predict_input_buf (&w, &h);
     else
         buf_fp32 = (float *)get_style_transfer_content_input_buf (&w, &h);
 
-    pui8 = buf_ui8 = (unsigned char *)malloc(w * h * 4);
+    if (buf_w != w || buf_h != h)
+    {
+        if (pui8)
+            free (pui8);
+        pui8 = (unsigned char *)malloc(w * h * 4);
+        buf_w = w;
+        buf_h = h;
+    }
 
-    draw_2d_texture (texid, 0, win_h - h, w, h, 1);
+    buf_ui8 = pui8;
 
-    glPixelStorei (GL_PACK_ALIGNMENT, 1);
+    draw_2d_texture_ex (srctex, 0, win_h - h, w, h, 1);
+
+    glPixelStorei (GL_PACK_ALIGNMENT, 4);
     glReadPixels (0, 0, w, h, GL_RGBA, GL_UNSIGNED_BYTE, buf_ui8);
 
-#if 0
-    /* convert UI8 [0, 255] ==> FP32 [-1, 1] */
-    float mean = 128.0f;
-    float std  = 128.0f;
-#else
     /* convert UI8 [0, 255] ==> FP32 [ 0, 1] */
     float mean =   0.0f;
     float std  = 255.0f;
-#endif
     for (y = 0; y < h; y ++)
     {
         for (x = 0; x < w; x ++)
@@ -79,7 +165,6 @@ feed_style_transfer_image(int is_predict, int texid, int win_w, int win_h)
         }
     }
 
-    free (pui8);
     return;
 }
 
@@ -223,22 +308,32 @@ main(int argc, char *argv[])
     int win_h = 540;
     int texid;
     int texw, texh, draw_x, draw_y, draw_w, draw_h;
-    int style_texid;
+    texture_2d_t captex = {0};
+    texture_2d_t styletex = {0};
     float style_ratio = -0.1f;
     double ttime[10] = {0}, interval, invoke_ms;
     int enable_camera = 1;
     UNUSED (argc);
     UNUSED (*argv);
+#if defined (USE_INPUT_VIDEO_DECODE)
+    int enable_video = 0;
+#endif
 
     /* gl2style_transfer [content_file_name] [style_file_name] */
     {
         int c;
-        const char *optstring = "x";
+        const char *optstring = "v:x";
 
         while ((c = getopt (argc, argv, optstring)) != -1)
         {
             switch (c)
             {
+#if defined (USE_INPUT_VIDEO_DECODE)
+            case 'v':
+                enable_video = 1;
+                input_name = optarg;
+                break;
+#endif
             case 'x':
                 enable_camera = 0;
                 break;
@@ -254,6 +349,7 @@ main(int argc, char *argv[])
             optind++;
         }
     }
+
     if (input_name == NULL)
         input_name = input_name_default;
     if (input_style_name == NULL)
@@ -273,18 +369,34 @@ main(int argc, char *argv[])
     glViewport (0, 0, win_w, win_h);
 #endif
 
+#if defined (USE_INPUT_VIDEO_DECODE)
+    /* initialize FFmpeg video decode */
+    if (enable_video && init_video_decode () == 0)
+    {
+        init_video_texture (&captex, input_name);
+        texw = captex.width;
+        texh = captex.height;
+        enable_camera = 0;
+    }
+    else
+#endif
 #if defined (USE_INPUT_CAMERA_CAPTURE)
     /* initialize V4L2 capture function */
     if (enable_camera && init_capture () == 0)
     {
-        /* allocate texture buffer for captured image */
-        get_capture_dimension (&texw, &texh);
-        texid = create_2d_texture (NULL, texw, texh);
-        start_capture ();
+        init_capture_texture (&captex);
+        texw = captex.width;
+        texh = captex.height;
     }
     else
 #endif
-    load_jpg_texture (input_name, &texid, &texw, &texh);
+    {
+        load_jpg_texture (input_name, &texid, &texw, &texh);
+        captex.texid  = texid;
+        captex.width  = texw;
+        captex.height = texh;
+        captex.format = pixfmt_fourcc ('R', 'G', 'B', 'A');
+    }
     adjust_texture (win_w, win_h, texw, texh, &draw_x, &draw_y, &draw_w, &draw_h);
 
     glClearColor (0.f, 0.f, 0.f, 1.0f);
@@ -295,17 +407,21 @@ main(int argc, char *argv[])
     style_predict_t style_predict[2] = {0};
     {
         int w, h;
-        load_jpg_texture (input_style_name, &style_texid, &w, &h);
+        load_jpg_texture (input_style_name, &texid, &w, &h);
+        styletex.texid  = texid;
+        styletex.width  = texw;
+        styletex.height = texh;
+        styletex.format = pixfmt_fourcc ('R', 'G', 'B', 'A');
 
         /* predict style of original image */
         glClear (GL_COLOR_BUFFER_BIT);
-        feed_style_transfer_image (1, texid, win_w, win_h);
+        feed_style_transfer_image (1, &captex, win_w, win_h);
         invoke_style_predict (&style_predict[0]);
         store_style_predict (&style_predict[0]);
 
         /* predict style of target image */
         glClear (GL_COLOR_BUFFER_BIT);
-        feed_style_transfer_image (1, style_texid, win_w, win_h);
+        feed_style_transfer_image (1, &styletex, win_w, win_h);
         invoke_style_predict (&style_predict[1]);
         store_style_predict (&style_predict[1]);
     }
@@ -327,10 +443,17 @@ main(int argc, char *argv[])
 
         glClear (GL_COLOR_BUFFER_BIT);
 
+#if defined (USE_INPUT_VIDEO_DECODE)
+        /* initialize FFmpeg video decode */
+        if (enable_video)
+        {
+            update_video_texture (&captex);
+        }
+#endif
 #if defined (USE_INPUT_CAMERA_CAPTURE)
         if (enable_camera)
         {
-            update_capture_texture (texid);
+            update_capture_texture (&captex);
         }
 #endif
 
@@ -349,7 +472,7 @@ main(int argc, char *argv[])
 
         /* feed style parameter and original image */
         feed_blend_style (&style_predict[0], &style_predict[1], style_ratio);
-        feed_style_transfer_image (0, texid, win_w, win_h);
+        feed_style_transfer_image (0, &captex, win_w, win_h);
 
         /* invoke pose estimation using TensorflowLite */
         ttime[2] = pmeter_get_time_ms ();
@@ -362,17 +485,17 @@ main(int argc, char *argv[])
         int transfered_texid = update_style_transfered_texture (&style_transfered);
 #if 0
         if (style_ratio < 0.0f)     /* render original content image */
-            draw_2d_texture (texid,  draw_x, draw_y, draw_w, draw_h, 0);
+            draw_2d_texture_ex (&captex, draw_x, draw_y, draw_w, draw_h, 0);
         else                        /* render style transformed image */
             draw_2d_texture (transfered_texid,  draw_x, draw_y, draw_w, draw_h, 0);
 #else
-        draw_2d_texture (texid, 0, 0, 750, 540, 0);
+        draw_2d_texture_ex (&captex, 0, 0, 750, 540, 0);
         draw_2d_texture (transfered_texid, 720, 0, 720, 540, 0);
 #endif        
         /* render the target style image */
         {
             float col_black[] = {1.0f, 1.0f, 1.0f, 1.0f};
-            draw_2d_texture (style_texid,  win_w - 200, 0, 200, 200, 0);
+            draw_2d_texture_ex (&styletex,  win_w - 200, 0, 200, 200, 0);
             draw_2d_rect (win_w - 200, 0, 200, 200, col_black, 2.0f);
         }
 
