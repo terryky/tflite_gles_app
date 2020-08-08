72197e8eedd8dd40e65f2d7abab5562bf084bffb
terryky <terryky1220@gmail.com>
2020-08-08 11:30:02 +0900

    update camera capture

--- gl2text_detection/camera_capture.c
+++ gl2text_detection/camera_capture.c
@@ -17,6 +17,7 @@ static pthread_t    s_capture_thread;
 static void         *s_capture_buf = NULL;
 static capture_dev_t *s_cap_dev;
 static int          s_capture_w, s_capture_h;
+static int          s_capcrop_w, s_capcrop_h;
 static unsigned int s_capture_fmt;
 
 #define _max(A, B)    ((A) > (B) ? (A) : (B))
@@ -25,7 +26,7 @@ static unsigned int s_capture_fmt;
 
 #if defined(USE_YUYV_TO_RGB_CONVERSION)
 static int
-convert_to_rgba8888 (void *buf, int cap_w, int cap_h, unsigned int fmt)
+convert_to_rgba8888 (void *buf, int ofstx, int ofsty, int cap_w, int cap_h, unsigned int fmt)
 {
     int x, y;
 
@@ -37,9 +38,12 @@ convert_to_rgba8888 (void *buf, int cap_w, int cap_h, unsigned int fmt)
     if (fmt == v4l2_fourcc ('Y', 'U', 'Y', 'V'))
     {
         unsigned char *src8 = buf;
+        unsigned char *srcline = buf;
         unsigned char *dst8 = s_capture_buf;
         for (y = 0; y < cap_h; y ++)
         {
+            src8 = &srcline[y * 2 * s_capture_w];
+            src8 += ofstx * 2;
             for (x = 0; x < cap_w; x += 2)
             {
                 int y0 = *src8 ++;
@@ -92,7 +96,7 @@ convert_to_rgba8888 (void *buf, int cap_w, int cap_h, unsigned int fmt)
 #else
 
 static int
-copy_yuyv_image (void *buf, int cap_w, int cap_h, unsigned int fmt)
+copy_yuyv_image (void *buf, int ofstx, int ofsty, int cap_w, int cap_h, unsigned int fmt)
 {
     if (s_capture_buf == NULL)
     {
@@ -101,7 +105,18 @@ copy_yuyv_image (void *buf, int cap_w, int cap_h, unsigned int fmt)
 
     if (fmt == v4l2_fourcc ('Y', 'U', 'Y', 'V'))
     {
-        memcpy (s_capture_buf, buf, cap_w * cap_h * 2);
+        unsigned char *src8 = buf;
+        unsigned char *dst8 = s_capture_buf;
+        for (int ydst = 0; ydst < cap_h; ydst ++)
+        {
+            int ysrc = ydst + ofsty;
+            unsigned char *srcline = &src8[ysrc * 2 * s_capture_w];
+            unsigned char *dstline = &dst8[ydst * 2 * cap_w];
+
+            srcline += ofstx * 2;
+
+            memcpy (dstline, srcline, cap_w * 2);
+        }
     }
     else
     {
@@ -120,12 +135,15 @@ capture_thread_main ()
 
     while (1)
     {
+        int ofstx = (s_capture_w - s_capcrop_w) * 0.5f;
+        int ofsty = (s_capture_h - s_capcrop_h) * 0.5f;
+
         capture_frame_t *frame = v4l2_acquire_capture_frame (s_cap_dev);
 
 #if defined(USE_YUYV_TO_RGB_CONVERSION)
-        convert_to_rgba8888 (frame->vaddr, s_capture_w, s_capture_h, s_capture_fmt);
+        convert_to_rgba8888 (frame->vaddr, ofstx, ofsty, s_capcrop_w, s_capcrop_h, s_capture_fmt);
 #else
-        copy_yuyv_image (frame->vaddr, s_capture_w, s_capture_h, s_capture_fmt);
+        copy_yuyv_image (frame->vaddr, ofstx, ofsty, s_capcrop_w, s_capcrop_h, s_capture_fmt);
 #endif
         v4l2_release_capture_frame (s_cap_dev, frame);
     }
@@ -158,14 +176,19 @@ init_capture ()
     s_capture_w = cap_w;
     s_capture_h = cap_h;
 
+    if (cap_w > cap_h)
+        s_capcrop_w = s_capcrop_h = cap_h;
+    else
+        s_capcrop_w = s_capcrop_h = cap_w;
+
     return 0;
 }
 
 int 
 get_capture_dimension (int *width, int *height)
 {
-    *width  = s_capture_w;
-    *height = s_capture_h;
+    *width  = s_capcrop_w;
+    *height = s_capcrop_w;
 
     return 0;
 }
