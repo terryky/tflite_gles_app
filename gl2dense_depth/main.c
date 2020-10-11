5cd116fcaec6c3c33a300695e94b53a73182f398
terryky <terryky1220@gmail.com>
2020-10-12 00:26:10 +0900

    add render_depth_3d()

--- gl2dense_depth/main.c
+++ gl2dense_depth/main.c
@@ -1,6 +1,6 @@
 /* ------------------------------------------------ *
  * The MIT License (MIT)
- * Copyright (c) 2019 terryky1220@gmail.com
+ * Copyright (c) 2020 terryky1220@gmail.com
  * ------------------------------------------------ */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,8 +17,13 @@
 #include "tflite_dense_depth.h"
 #include "camera_capture.h"
 #include "video_decode.h"
+#include "render_dense_depth.h"
+#include "touch_event.h"
+#include "render_imgui.h"
 
 #define UNUSED(x) (void)(x)
+
+static imgui_data_t s_gui_prop = {0};
 
 
 #if defined (USE_INPUT_CAMERA_CAPTURE)
@@ -203,6 +208,104 @@ render_depth_image (texture_2d_t *srctex, int ofstx, int ofsty, int texw, int te
     glDeleteTextures (1, &animtex.texid);
 }
 
+
+static void
+render_depth_image_3d (texture_2d_t *srctex, int ofstx, int ofsty, int texw, int texh,
+                       dense_depth_result_t *dense_depth_ret)
+{
+    float mtxGlobal[16], mtxTouch[16];
+    static int s_is_first_render3d = 1;
+    static mesh_obj_t s_depth_mesh;
+
+    get_touch_event_matrix (mtxTouch);
+    matrix_identity (mtxGlobal);
+    matrix_translate (mtxGlobal, 0, 0, -s_gui_prop.camera_pos_z);
+    matrix_mult (mtxGlobal, mtxGlobal, mtxTouch);
+
+    float *depthmap = dense_depth_ret->depthmap;
+    int depthmap_w  = dense_depth_ret->depthmap_dims[0];
+    int depthmap_h  = dense_depth_ret->depthmap_dims[1];
+
+    /* create mesh object */
+    if (s_is_first_render3d)
+    {
+        create_mesh (&s_depth_mesh, depthmap_w - 1, depthmap_h - 1);
+        s_is_first_render3d = 0;
+    }
+    float *vtx = s_depth_mesh.vtx_array;
+    float *uv  = s_depth_mesh.uv_array;
+
+    /* create 3D vertex coordinate */
+    for (int y = 0; y < depthmap_h; y ++)
+    {
+        for (int x = 0; x < depthmap_w; x ++)
+        {
+            int   idx = (y * depthmap_w + x);
+            float d = depthmap[idx];
+
+            if (1)
+            {
+                d -= 0;//s_gui_prop.depth_min;
+                d /= 10;//s_gui_prop.depth_max;
+                d = (d * 2.0 - 1.0) * s_gui_prop.pose_scale_z;
+            }
+            else
+            {
+                //d = s_gui_prop.depth_max / d;   //  inf -> 1.0
+                //d = 2 - d;                      // -inf -> 1.0
+                //d = d * s_gui_prop.pose_scale_z;
+            }
+
+            vtx[3 * idx + 0] =  ((x / (float)depthmap_h) * 2.0f - 1.0f) * s_gui_prop.pose_scale_x;
+            vtx[3 * idx + 1] = -((y / (float)depthmap_h) * 2.0f - 1.0f) * s_gui_prop.pose_scale_y;
+            vtx[3 * idx + 2] =  d;
+
+            uv [2 * idx + 0] = x / (float)depthmap_w;
+            uv [2 * idx + 1] = y / (float)depthmap_h;
+        }
+    }
+    float colb[] = {1.0, 1.0, 1.0, 1.0};
+    draw_point_arrays (mtxGlobal, vtx, uv, depthmap_h * depthmap_w, srctex->texid, colb);
+
+    if (s_gui_prop.draw_axis)
+    {
+        /* (xyz)-AXIS */
+        for (int i = -1; i <= 1; i ++)
+        {
+            for (int j = -1; j <= 1; j ++)
+            {
+                float col_base[] = {0.1, 0.5, 0.5, 0.5};
+                float dx = s_gui_prop.pose_scale_x;
+                float dy = s_gui_prop.pose_scale_y;
+                float dz = s_gui_prop.pose_scale_z;
+
+                {
+                    float v0[3] = {-dx, i * dy, j * dz};
+                    float v1[3] = { dx, i * dy, j * dz};
+                    float col_red[] = {1.0, 0.0, 0.0, 1.0};
+                    float *col = (i == 0 && j == 0) ? col_red : col_base;
+                    draw_line (mtxGlobal, v0, v1, col);
+                }
+                {
+                    float v0[3] = {i * dx, -dy, j * dz};
+                    float v1[3] = {i * dx,  dy, j * dz};
+                    float col_green[] = {0.0, 1.0, 0.0, 1.0};
+                    float *col = (i == 0 && j == 0) ? col_green : col_base;
+                    draw_line (mtxGlobal, v0, v1, col);
+                }
+                {
+                    float v0[3] = {i * dx, j * dy, -dz};
+                    float v1[3] = {i * dx, j * dy,  dz};
+                    float col_blue[] = {0.0, 0.0, 1.0, 1.0};
+                    float *col = (i == 0 && j == 0) ? col_blue : col_base;
+                    draw_line (mtxGlobal, v0, v1, col);
+                }
+            }
+        }
+    }
+}
+
+
 /* Adjust the texture size to fit the window size
  *
  *                      Portrait
@@ -246,6 +349,57 @@ adjust_texture (int win_w, int win_h, int texw, int texh,
     *dh = (int)scaled_h;
 }
 
+void
+mousemove_cb (int x, int y)
+{
+#if defined (USE_IMGUI)
+    imgui_mousemove (x, y);
+    if (imgui_is_anywindow_hovered ())
+        return;
+#endif
+
+    touch_event_move (0, x, y);
+}
+
+void
+button_cb (int button, int state, int x, int y)
+{
+#if defined (USE_IMGUI)
+    imgui_mousebutton (button, state, x, y);
+#endif
+
+    if (state)
+        touch_event_start (0, x, y);
+    else
+        touch_event_end (0);
+}
+
+void
+keyboard_cb (int key, int state, int x, int y)
+{
+}
+
+void
+setup_imgui (int win_w, int win_h)
+{
+    egl_set_motion_func (mousemove_cb);
+    egl_set_button_func (button_cb);
+    egl_set_key_func    (keyboard_cb);
+
+    init_touch_event (win_w, win_h);
+
+#if defined (USE_IMGUI)
+    init_imgui (win_w, win_h);
+#endif
+
+    s_gui_prop.pose_scale_x = 100.0f;
+    s_gui_prop.pose_scale_y = 100.0f;
+    s_gui_prop.pose_scale_z = 100.0f;
+    s_gui_prop.camera_pos_z = 200.0f;
+    s_gui_prop.draw_axis    = 0;
+    s_gui_prop.draw_pmeter  = 1;
+}
+
 
 /*--------------------------------------------------------------------------- *
  *      M A I N    F U N C T I O N
@@ -256,12 +410,11 @@ main(int argc, char *argv[])
     char input_name_default[] = "assets/pexels.jpg";
     char *input_name = NULL;
     int count;
-    int win_w = 800;
-    int win_h = 800;
-    int texid;
+    int win_w = 900;
+    int win_h = 900;
     int texw, texh, draw_x, draw_y, draw_w, draw_h;
     texture_2d_t captex = {0};
-    double ttime[10] = {0}, interval, invoke_ms0 = 0;
+    double ttime[10] = {0}, interval, invoke_ms;
     int use_quantized_tflite = 0;
     int enable_camera = 1;
     UNUSED (argc);
@@ -303,16 +456,18 @@ main(int argc, char *argv[])
     if (input_name == NULL)
         input_name = input_name_default;
 
-    egl_init_with_platform_window_surface (2, 0, 0, 0, win_w * 2, win_h);
+    egl_init_with_platform_window_surface (2, 8, 0, 0, win_w * 2, win_h);
 
     init_2d_renderer (win_w, win_h);
     init_pmeter (win_w, win_h, 500);
     init_dbgstr (win_w, win_h);
+    init_cube ((float)win_w / (float)win_h);
 
     init_tflite_dense_depth (use_quantized_tflite);
+    setup_imgui (win_w * 2, win_h);
 
 #if defined (USE_GL_DELEGATE) || defined (USE_GPU_DELEGATEV2)
-    /* we need to recover framebuffer because GPU Delegate changes the FBO binding. */
+    /* we need to recover framebuffer because GPU Delegate changes the FBO binding */
     glBindFramebuffer (GL_FRAMEBUFFER, 0);
     glViewport (0, 0, win_w, win_h);
 #endif
@@ -339,6 +494,7 @@ main(int argc, char *argv[])
     else
 #endif
     {
+        int texid;
         load_jpg_texture (input_name, &texid, &texw, &texh);
         captex.texid  = texid;
         captex.width  = texw;
@@ -347,14 +503,17 @@ main(int argc, char *argv[])
     }
     adjust_texture (win_w, win_h, texw, texh, &draw_x, &draw_y, &draw_w, &draw_h);
 
+
     glClearColor (0.f, 0.f, 0.f, 1.0f);
+
 
     /* --------------------------------------- *
      *  Render Loop
      * --------------------------------------- */
+    dense_depth_result_t dense_depth_result = {0};
+    int is_first = 1;
     for (count = 0; ; count ++)
     {
-        dense_depth_result_t   dense_depth_result = {0};
         char strbuf[512];
 
         PMETER_RESET_LAP ();
@@ -384,38 +543,61 @@ main(int argc, char *argv[])
         /* --------------------------------------- *
          *  Dense Depth
          * --------------------------------------- */
-        feed_dense_depth_image (&captex, win_w, win_h);
+        invoke_ms = 0;
+        if (is_first)
+        {
+            feed_dense_depth_image (&captex, win_w, win_h);
 
-        ttime[2] = pmeter_get_time_ms ();
-        invoke_dense_depth (&dense_depth_result);
-        ttime[3] = pmeter_get_time_ms ();
-        invoke_ms0 = ttime[3] - ttime[2];
+            ttime[2] = pmeter_get_time_ms ();
+            invoke_dense_depth (&dense_depth_result);
+            ttime[3] = pmeter_get_time_ms ();
+            invoke_ms = ttime[3] - ttime[2];
+
+            is_first = 0;
+        }
 
         /* --------------------------------------- *
          *  render scene (left half)
          * --------------------------------------- */
-        glClear (GL_COLOR_BUFFER_BIT);
+        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         draw_2d_texture_ex (&captex, draw_x, draw_y, draw_w, draw_h, 0);
 
+        {
+            float aspect = (float)dense_depth_result.depthmap_dims[0] / 
+                           (float)dense_depth_result.depthmap_dims[1];
+            int dw = 200 * aspect;
+            int dh = 200;
+            int dx = win_w - dw - 10;
+            int dy = 10;
+            float col_white[] = {1.0f, 1.0f, 1.0f, 1.0f};
+
+            render_depth_image (&captex, dx, dy, dw, dh, &dense_depth_result);
+            draw_2d_rect (dx, dy, dw, dh, col_white, 2.0f);
+        }
 
         /* --------------------------------------- *
          *  render scene  (right half)
          * --------------------------------------- */
         glViewport (win_w, 0, win_w, win_h);
-        draw_2d_texture_ex (&captex, draw_x, draw_y, draw_w, draw_h, 0);
 
-        render_depth_image (&captex, draw_x, draw_y, draw_w, draw_h, &dense_depth_result);
+        render_depth_image_3d (&captex, draw_x, draw_y, draw_w, draw_h, &dense_depth_result);
 
         /* --------------------------------------- *
          *  post process
          * --------------------------------------- */
         glViewport (0, 0, win_w, win_h);
-        draw_pmeter (0, 40);
 
-        sprintf (strbuf, "Interval:%5.1f [ms]\nTFLite0 :%5.1f [ms]",
-            interval, invoke_ms0);
+        if (s_gui_prop.draw_pmeter)
+        {
+            draw_pmeter (0, 40);
+        }
+
+        sprintf (strbuf, "Interval:%5.1f [ms]\nTFLite  :%5.1f [ms]", interval, invoke_ms);
         draw_dbgstr (strbuf, 10, 10);
 
+#if defined (USE_IMGUI)
+        invoke_imgui (&s_gui_prop);
+#endif
         egl_swap();
     }
 
