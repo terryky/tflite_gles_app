db2be388bf0d73cd21db4f34733833c938a7ab5e
terryky <terryky1220@gmail.com>
2020-12-14 00:32:21 +0900

    update script

--- tools/scripts/tf2.4/benchmark_build.sh
+++ tools/scripts/tf2.4/benchmark_build.sh
@@ -6,7 +6,20 @@
 #  https://github.com/tensorflow/tensorflow/tree/master/tensorflow/lite/tools/benchmark
 # -----------------------------------------------
 
-export TENSORFLOW_DIR=~/work/tensorflow
+# [install jdk]
+#   $ sudo apt install default-jdk
+#
+# [install bazel on aarch64 target (Raspi/Jetson)]
+#   $ git clone https://github.com/PINTO0309/Bazel_bin
+#   $ cd Bazel_bin/3.1.0/Raspbian_Debian_Buster_aarch64/openjdk-8-jdk
+#   $ ./install.sh
+#
+# [install bazel on x64_64]
+#   $ wget https://github.com/bazelbuild/bazel/releases/download/3.1.0/bazel-3.1.0-installer-linux-x86_64.sh
+#   $ chmod 755 bazel-3.1.0-installer-linux-x86_64.sh
+#   $ sudo ./bazel-3.1.0-installer-linux-x86_64.sh
+
+export TENSORFLOW_DIR=${HOME}/work/tensorflow
 
 cd ${TENSORFLOW_DIR}
 
