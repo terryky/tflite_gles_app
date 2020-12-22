64746af64a0e44d022209205c540f0c2e56f803a
terryky <terryky1220@gmail.com>
2020-12-22 22:53:22 +0900

    refactoring

--- common/util_texture.h
+++ common/util_texture.h
@@ -25,12 +25,12 @@ uint32_t create_2d_texture (void *imgbuf, int width, int height);
 
 int create_2d_texture_ex (texture_2d_t *tex2d, void *imgbuf, int w, int h, uint32_t fmt);
 
-#if defined (USE_INPUT_CAMERA_CAPTURE2)
+#if defined (USE_INPUT_CAMERA_CAPTURE)
 int  create_capture_texture (texture_2d_t *captex);
 void update_capture_texture (texture_2d_t *captex);
 #endif
 
-#if defined (USE_INPUT_VIDEO_DECODE2)
+#if defined (USE_INPUT_VIDEO_DECODE)
 int  create_video_texture (texture_2d_t *vidtex, const char *fname);
 void update_video_texture (texture_2d_t *vidtex);
 #endif
