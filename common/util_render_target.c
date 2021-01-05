78c8a10d92bc23b9314cc83c61679a73da230d5f
terryky <terryky1220@gmail.com>
2021-01-06 01:05:36 +0900

    refactoring

--- common/util_render_target.c
+++ common/util_render_target.c
@@ -6,37 +6,64 @@
 #include <string.h>
 #include <math.h>
 #include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
 #include "assertgl.h"
-#include "util_shader.h"
-#include "util_matrix.h"
 #include "util_render_target.h"
+
+#define UNUSED(x) (void)(x)
 
 
 int
-create_render_target (render_target_t *rtarget, int w, int h, unsigned int flag)
+create_render_target (render_target_t *rtarget, int w, int h, unsigned int flags)
 {
-    GLuint fbo_id = 0, tex_id = 0;
-
-    if (flag)
+    /* texture for color */
+    GLuint tex_c = 0;
+    if (flags & RTARGET_COLOR)
     {
-        glGenTextures (1, &tex_id);
-        glBindTexture (GL_TEXTURE_2D, tex_id);
-        glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+        glGenTextures (1, &tex_c);
+        glBindTexture (GL_TEXTURE_2D, tex_c);
         glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameterf (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glBindTexture (GL_TEXTURE_2D, 0);
-
-        glGenFramebuffers (1, &fbo_id);
+        glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
     }
 
-    rtarget->texid  = tex_id;
-    rtarget->fboid  = fbo_id;
-    rtarget->width  = w;
-    rtarget->height = h;
+    /* texture for depth */
+    GLuint tex_z = 0;
+    if (flags & RTARGET_DEPTH)
+    {
+        glGenTextures (1, &tex_z);
+        glBindTexture (GL_TEXTURE_2D, tex_z);
+        glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexImage2D (GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, w, h, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, NULL);
+    }
 
-    GLASSERT ();
+    glBindTexture (GL_TEXTURE_2D, 0);
+
+    GLuint fbo = 0;
+    if (flags)
+    {
+        glGenFramebuffers (1, &fbo);
+        glBindFramebuffer (GL_FRAMEBUFFER, fbo);
+        glFramebufferTexture2D (GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex_c, 0);
+        glFramebufferTexture2D (GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,  GL_TEXTURE_2D, tex_z, 0);
+    }
+
+    glBindFramebuffer (GL_FRAMEBUFFER, 0);
+
+    memset (rtarget, 0, sizeof (*rtarget));
+    rtarget->texc_id = tex_c;
+    rtarget->texz_id = tex_z;
+    rtarget->fbo_id  = fbo;
+    rtarget->width   = w;
+    rtarget->height  = h;
+
+    GLASSERT();
+
     return 0;
 }
 
@@ -44,33 +71,59 @@ create_render_target (render_target_t *rtarget, int w, int h, unsigned int flag)
 int
 destroy_render_target (render_target_t *rtarget)
 {
-    GLuint fbotexid = rtarget->texid;
-    GLuint fboid    = rtarget->fboid;
+    glDeleteTextures (1, &rtarget->texc_id);
+    glDeleteTextures (1, &rtarget->texz_id);
+    glDeleteFramebuffers (1, &rtarget->fbo_id);
+    memset (rtarget, 0, sizeof (*rtarget));
 
-    glDeleteTextures (1, &fbotexid);
-    glDeleteFramebuffers (1, &fboid);
+    GLASSERT();
 
-    GLASSERT ();
     return 0;
 }
 
 int
 set_render_target (render_target_t *rtarget)
 {
-    if (rtarget->fboid > 0)
-    {
-        glBindFramebuffer (GL_FRAMEBUFFER, rtarget->fboid);
-        glFramebufferTexture2D (GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, rtarget->texid, 0);
-    }
-    else
-    {
-        glBindFramebuffer (GL_FRAMEBUFFER, 0);
-    }
-
+    glBindFramebuffer (GL_FRAMEBUFFER, rtarget->fbo_id);
     glViewport (0, 0, rtarget->width, rtarget->height);
     glScissor  (0, 0, rtarget->width, rtarget->height);
 
-    GLASSERT ();
+    GLASSERT();
 
-  return 0;
+    return 0;
 }
+
+int
+get_render_target (render_target_t *rtarget)
+{
+    GLuint tex_c, tex_z, fbo;
+    int    viewport[4];
+
+    memset (rtarget, 0, sizeof (*rtarget));
+
+    glGetIntegerv (GL_FRAMEBUFFER_BINDING, (void *)&fbo);
+    if (fbo > 0)
+    {
+        glGetFramebufferAttachmentParameteriv (GL_FRAMEBUFFER,
+                                               GL_COLOR_ATTACHMENT0,
+                                               GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME,
+                                               (void *)&tex_c);
+
+        glGetFramebufferAttachmentParameteriv (GL_FRAMEBUFFER,
+                                               GL_DEPTH_ATTACHMENT,
+                                               GL_FRAMEBUFFER_ATTACHMENT_OBJECT_NAME,
+                                               (void *)&tex_z);
+        rtarget->fbo_id  = fbo;
+        rtarget->texc_id = tex_c;
+        rtarget->texz_id = tex_z;
+    }
+
+    glGetIntegerv (GL_VIEWPORT, viewport);
+    rtarget->width  = viewport[2];
+    rtarget->height = viewport[3];
+
+    GLASSERT();
+
+    return 0;
+}
+
--- common/util_render_target.h
+++ common/util_render_target.h
@@ -2,22 +2,36 @@
  * The MIT License (MIT)
  * Copyright (c) 2019 terryky1220@gmail.com
  * ------------------------------------------------ */
-#ifndef _UTIL_RENDER_TARGET_H_
-#define _UTIL_RENDER_TARGET_H_
+#ifndef UTIL_RENDER_TARGET_H
+#define UTIL_RENDER_TARGET_H
 
-typedef struct render_target_t
+
+#define RTARGET_DEFAULT     (0 << 0)
+#define RTARGET_COLOR       (1 << 0)
+#define RTARGET_DEPTH       (1 << 1)
+
+typedef struct _render_target_t
 {
-  int texid;
-  int fboid;
-  int width;
-  int height;
+    GLuint texc_id; /* color */
+    GLuint texz_id; /* depth */
+    GLuint fbo_id;
+    int width;
+    int height;
 } render_target_t;
 
-#define RENDER_TARGET_COLOR (1 << 0)
-#define RENDER_TARGET_DEPTH (1 << 1)
 
-int create_render_target (render_target_t *rtarget, int w, int h, unsigned int flag);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int create_render_target (render_target_t *rtarget, int w, int h, unsigned int flags);
 int destroy_render_target (render_target_t *rtarget);
 int set_render_target (render_target_t *rtarget);
+int get_render_target (render_target_t *rtarget);
+int blit_render_target (render_target_t *rtarget_src, int x, int y, int w, int h);
 
-#endif /* _UTIL_RENDER_TARGET_H_ */
+#ifdef __cplusplus
+}
+#endif
+#endif /* UTIL_RENDER_TARGET_H */
